2e6b71baebb91e5e393b333e523cf33f22856b5e
aronresni <aronresni@gmail.com>
2024-03-11 23:30:36 -0300

    uploaded image

--- src/components/macbookScroll.tsx
+++ src/components/macbookScroll.tsx
@@ -1,21 +1,18 @@
 import React from "react";
 import { MacbookScroll } from "../components/ui/macbook-scroll";
-
 import Link from "next/link";
-
 const MacbookScrollDemo = () => {
     return (
         <div className="overflow-hidden dark:bg-black bg-white w-full">
             <MacbookScroll
                 title={
                     <span>
-                        This Macbook is built with Tailwindcss. <br /> No kidding.
+                        Maneja todo tu negocio desde una sola App
                     </span>
                 }
-           
-                src={`/linear.webp`}
-                showGradient={false}
+        
             />
+        
         </div>
     );
 };
